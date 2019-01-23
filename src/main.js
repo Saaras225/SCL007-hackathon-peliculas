735457347e119fe9232218870d6d000a278a6ccb
GloriaMoraga <silmeriel@gail.com>
2019-01-23 01:00:35 -0300

    show movies in html from  mainjs

--- src/main.js
+++ src/main.js
@@ -2,43 +2,49 @@
 
 
 // Probando Data desde OMDB, solo funcion Data con ¿console Log?
-const dataMovie =["tt1201607"]
-let data;
- fetch('https://www.omdbapi.com/?i='+ dataMovie + '&apikey=cc1c0d5d')
+const dataMovie =['tt1201607', 'tt0076759', 'tt0266697', 'tt1201607', 'tt0076759', 'tt0266697','tt1201607', 'tt0076759', 'tt0266697']
+
+const randomMovie = dataMovie.map( function(item) {
+fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=cc1c0d5d')
   .then(res => res.json())
    .then(Data => {
-     
-    data = Data;
-   
- })
-
-const containerRoot = document.getElementById('allMovies');
-const showData = (data) => {
-  console.log(data);
-  let result = '';
+         document.getElementById('allMovies').innerHTML +=  `
+         <div class="col s12 m4 movie-card">
+                                <div class="card small center-align white">
+                                <div class="card-action">
+                                        <a href="#">${Data.Title}</a>
+                                    </div>
+                                    <div class="card-content white-text">
+                                        <img class="responsive-img" src="${Data.Poster}">
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                            `
   
- data.forEach(element => {
-   // element --> DATA[i]
-  //  console.log(element);
 
-     result = containerRoot.innerHTML += `
-     <div><img src="${element.Poster}">
-           <h2>${element.Title}</h2>
+})
+
+})
+
+
+
+
+// const showData = (data) => {
+//   console.log(data)
+//  data.forEach(element => {
+//    // element --> DATA[i]
+//    console.log(element)
+//    document.getElementById('allMovies').innerHTML +=  `
+//      <div> <img src="${element.Poster}">
+//           <h2>${element.Title}</h2>
+           
       
-     </div>`
+//      </div>`
    
- });
- return result;
-}
-
-window.onload = showData(data);
-
-
-
-
-
-
-
+//  });
+ 
+// }
 
 
 
