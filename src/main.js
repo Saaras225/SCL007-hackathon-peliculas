84f26cf8c7f3d4cfeabee87dd010d3f9b3accced
GloriaMoraga <silmeriel@gail.com>
2019-01-23 01:53:47 -0300

    style card mainjs

--- src/main.js
+++ src/main.js
@@ -2,32 +2,60 @@
 
 
 // Probando Data desde OMDB, solo funcion Data con ¿console Log?
-const dataMovie =['tt1201607', 'tt0076759', 'tt0266697', 'tt1201607', 'tt0076759', 'tt0266697','tt1201607', 'tt0076759', 'tt0266697']
+const dataMovie =['tt5164214', 'tt0451279', 'tt0266697', 'tt2326554', 'tt1289401', 'tt1392190','tt0147800', 'tt0076759', 'tt0090605']
 
 const randomMovie = dataMovie.map( function(item) {
 fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=cc1c0d5d')
   .then(res => res.json())
    .then(Data => {
+
          document.getElementById('allMovies').innerHTML +=  `
          <div class="col s12 m4 movie-card">
                                 <div class="card small center-align white">
-                                <div class="card-action">
-                                        <a href="#">${Data.Title}</a>
-                                    </div>
+                               
                                     <div class="card-content white-text">
                                         <img class="responsive-img" src="${Data.Poster}">
                                     </div>
+
+                                    <div class="card-action">
+                                    <a href="#">${Data.Title}</a>
+                                </div>
                                     
                                 </div>
                             </div>
                             `
   
-
+                        
+                            
 })
 
 })
 
 
+
+
+// selectHouse.addEventListener('change', () => {
+//   let condition = selectHouse.options[selectHouse.selectedIndex].text;
+//   let filtered = filterHouse(DATA, condition);
+//   console.log(filtered)
+//   // limpio div
+//   containerRoot.innerHTML = '';
+
+//   filtered.forEach(element => {
+//     containerRoot.innerHTML += `
+//     <div>
+//       <div class="card">
+//         <div class="box">
+//           <div class="img">
+//               <img src="${element.image}">
+//           </div>
+//           <h2>${element.name}<br><span>${element.house}</span></h2>
+//           <p>Patronus: ${element.patronus}</p>
+//         </div>
+//       </div>
+//     </div>` 
+//   })
+// })
 
 
 // const showData = (data) => {
@@ -45,7 +73,6 @@ fetch('https://www.omdbapi.com/?i='+ item  + '&apikey=cc1c0d5d')
 //  });
  
 // }
-
 
 
 
